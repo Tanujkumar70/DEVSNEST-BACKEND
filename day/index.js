f038a10da24b3536a8e6fab34c7408960993813c
Tanujkumar70 <84555067+Tanujkumar70@users.noreply.github.com>
2021-09-17 00:37:13 +0530

    Add files via upload

---
+++ day/index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+fs.mkdir("folder_async", (err) => {
+    console.log("created folder");
+});
+
+fs.writeFile("./folder_async/async_file.js", "imma type some shit here", (err) => {
+    console.log("file created");
+})
+
+fs.appendFile("./folder_async/async_file.js", "hemlo,appended this just fineee.", (err) => {
+    console.log("stuff appended");
+})
+
+fs.readFile("./folder_async/async_file.js", "utf-8", (err, data) => {
+    console.log(data);
+})
+
+fs.rename("./folder_async/async_file.js", "./folder_async/async_renamed.js", (err) => {
+    console.log("file renamed");
+})
+
